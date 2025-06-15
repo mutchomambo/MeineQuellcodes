d8ef70fb753b81d5c4dee83f429cb336512fef2e
matija <matija@matija-manjaroLinux>
2025-06-15 18:29:25 +0200

    Durschnittsrechner

--- MeineQuellcodes/src/codes/DurchschnittsRechner.java
+++ MeineQuellcodes/src/codes/DurchschnittsRechner.java
@@ -1,10 +1,40 @@
 package codes;
 
+import java.util.Scanner;
+
 public class DurchschnittsRechner {
 
 	public static void main (String[]args) {
+		Scanner eingabe = new Scanner(System.in);
+		begrüßung("Durchschnittsrechner  ");
+		berechnung(eingabe);
+	}
+	public static void begrüßung (String x) {
 		
-		System.out.println("Hello");
+		for (int i = 0 ; i < 5; i++) {
+			System.out.println();
+			for (int j = 0; j < 5; j++) {
+				System.out.print(x);
+			}
+		}
+		System.out.printf("%n%n");
+		System.err.println("Für Ergebnis bitte 00 und Enter");
+		System.out.printf("%n%n");
 	}
 	
+	public static void berechnung (Scanner eingabe) {
+		int zählerEingabe = 0;
+		double zahl, summe = 0;
+		do {
+			
+			zählerEingabe = zählerEingabe +1;
+			System.out.printf("%d. Zahl Eingabe: ", zählerEingabe);
+			zahl = eingabe.nextDouble();
+			summe += zahl;
+			
+			
+		}while(zahl != 00);
+		summe /= (zählerEingabe-1);
+		System.out.printf("Der Durschnitt von den %d. Zahlen ist %.2f ", zählerEingabe, summe);
+	}
 }
